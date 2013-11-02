13f00a489b312e15171324a6ee757323dc047aae
Kendall Park <kendall.park@cph.org>
2013-11-02 18:08:09 -0500

    Added rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quanta::Application.config.secret_key_base = '80a64c6d08b2bedda80726eaf138e5eb561d6a28d72a9042faff9a66b7de08b87654599e6e250613d301d09283ad010d931686b44518aaf825b439c9938a4ebe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Quanta::Application.config.session_store :cookie_store, key: '_quanta_session'
